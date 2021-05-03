fb4180efa2e3abf1d8f14f283258ed5f0445a2ac
GREBIAR-Git <nikitos.muzalevskiY@mail.ru>
2021-05-03 04:15:00 +0300

    Финал

---
+++ Старые версии/lab 5-8/blocks/registrations.php
@@ -0,0 +1,68 @@
+<?php
+if(!isset($_GET["IDP"]))
+{
+	echo '
+	<form class = "registration">
+		<fieldset class="clearfixup">
+			<button type="button" class="sign_in" id="sign_in1" onclick="sign_in();">Войти</button><a class="slash"> / </a>
+			<button type="button" class="sign_up" id="sign_up1" onclick="sign_up();">Зарегистрироваться</button>
+			<center><input name="sign" checked type="radio" value="in">
+	    	<input name="sign" type="radio" value="up"></center>
+		</fieldset>
+		<fieldset class="clearfix">
+			<input class="login" type="text" name="lg" onBlur="" required onFocus="" placeholder="Логин" maxlength="28" minlength="6">
+			<input class="password" type="password" name="pswrd" required onBlur="" onFocus="" placeholder="Пароль" maxlength="20" minlength="6">
+			<p>';
+			if(isset($_GET["News"]))
+			{
+				echo '<input type="hidden" name="News">';
+			}
+			else if(isset($_GET["Counter"]))
+			{
+				echo '<input type="hidden" name="Counter">';
+			}
+			else if(isset($_GET["Download"]))
+			{
+				echo '<input type="hidden" name="Download">';
+			}
+			echo '
+				<button class="sumbmit" type="sumbmit" name="next" onclick="Next();">Далее</button>
+			</p>
+		</fieldset>
+	</form>';
+}
+else if (isset($_GET["News"]) or isset($_GET["Counter"]) or isset($_GET["Download"]))
+{
+	echo '
+	<form class = "registration">
+		<fieldset class="clearfixup">
+	    	<p class="textReg">Вы : ';
+	    	Connect();
+			global $link;
+			global $result;
+			$IDP = htmlspecialchars($_GET["IDP"]);
+			$sql = "SELECT * FROM `accounts` WHERE `IDP` = '$IDP'";
+			$result = mysqli_query($link, $sql) or die("Ошибка " . mysqli_error($link));
+			$rows = mysqli_num_rows($result);
+			$row = mysqli_fetch_row($result);
+			echo $row[2];
+			Close();
+	    	echo '</p>
+		</fieldset>
+		<fieldset class="clearfix">';
+			if(isset($_GET["News"]))
+			{
+				echo '<button class="sumbmit" type="sumbmit" id="Exit" name="News">Выход</button>';
+			}
+			else if(isset($_GET["Counter"]))
+			{
+				echo '<button class="sumbmit" type="sumbmit" id="Exit" name="Counter">Выход</button>';
+			}
+			else if(isset($_GET["Download"]))
+			{
+				echo '<button class="sumbmit" type="sumbmit" id="Exit" name="Download">Выход</button>';
+			}
+		echo '</fieldset>
+	</form>';
+}
+?>
---
+++ Старые версии/lab 5-8/function/js/Counter.js
@@ -0,0 +1,41 @@
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {'total': t,'days': days,'hours': hours,'minutes': minutes,'seconds': seconds};
+}
+
+function countdown() {
+    endtime = '6/1/2021';
+    var days = document.getElementById("days");
+    var hours = document.getElementById("hours");
+    var minutes = document.getElementById("minutes");
+    var seconds = document.getElementById("seconds");
+    function updateClock() {
+        var t = getTimeRemaining(endtime);
+        days.innerHTML = t.days;
+        hours.innerHTML = ('0' + t.hours).slice(-2);
+        minutes.innerHTML = ('0' + t.minutes).slice(-2);
+        seconds.innerHTML = ('0' + t.seconds).slice(-2);
+        if (t.total <= 0) 
+        {
+          clearInterval(timeinterval);
+        }
+    }
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+}
+
+countdown();
+
+function ModalOn(){
+    var modal = document.getElementById('myModal');
+    modal.style.display = "block";
+}
+
+function ModalOff() {
+    var modal = document.getElementById('myModal');
+    modal.style.display = "none";
+}
