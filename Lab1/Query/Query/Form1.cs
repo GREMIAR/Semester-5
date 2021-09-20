dd9c13f3b9f6d19f2e9a77d99372f95628f7fb5a
GREMIAR <70177742+GREMIAR@users.noreply.github.com>
2021-09-20 21:50:53 +0300

    вроде работает

---
+++ Lab1/Query/Query/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Queue
+{
+    public partial class Form1 : Form
+    {
+        Queue queue;
+        ListOur list;
+        public Form1()
+        {
+            InitializeComponent();
+            queue = new Queue(5);
+            list = new ListOur();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Node node = new Node(textBox1.Text, int.Parse(textBox2.Text));
+            if (radioButton1.Checked)
+            {
+                queue.EnQueue(node);
+                WriteQueue();
+            }
+            else
+            {
+                list.Add(node);
+                WriteList();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                queue.Step();
+                WriteQueue();
+            }
+            else
+            {
+                list.Step();
+                WriteList();
+            }
+        }
+
+        void WriteQueue()
+        {
+            richTextBox1.Clear();
+            Node now = queue.Head;
+            for (int i = 0; i < queue.Count; i++)
+            {
+                richTextBox1.Text += now.Data + " " + now.Time + "\n";
+                now = now.Next;
+            }
+        }
+
+        void WriteList()
+        {
+            richTextBox1.Clear();
+            Node now = list.Head;
+            while (now !=null)
+            {
+                richTextBox1.Text += now.Data + " " + now.Time + "\n";
+                now = now.Next;
+            }
+        }
+    }
+}
---
+++ Lab1/Query/Query/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Queue
+{
+    public class Node
+    {
+        public string Data { get; set; }
+        public int Time { get; set; }
+        public Node Next { get; set; }
+        public Node(string data, int time)
+        {
+            if (data.Length == 4)
+            {
+                Data = data;
+                Time = time;
+                Next = null;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("Не из 4");
+            }
+        }
+    }
+}
