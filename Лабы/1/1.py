dac30e5e34a6d3bf9b2723e561f4abc0de3999c6
GREBIAR <nikitos.muzalevskiy@mail.ru>
2021-09-22 19:24:19 +0300

    fix

--- Лабы/1/1.py
+++ Лабы/1/1.py
@@ -1,16 +1,15 @@
 import math as m
 
 def InputArgument(argument):
-	return int(input("Введите переменную "+argument+": "))
+	while True:
+		try:
+			return int(input("Введите переменную "+argument+": "))
+		except:
+			print("Что-то при вводе переменной " + argument + " пошло не так")
 
-while True:
-	try:
-		a = InputArgument("a")
-		n = InputArgument("n")
-		x = InputArgument("x")
-		break
-	except :
-		print("Что-то пошло не так, повторите пожалуйста")
+a = InputArgument("a")
+n = InputArgument("n")
+x = InputArgument("x")
 try:
 	answer = 5*m.pow(a,n*x)/(n+x)-(m.sqrt(m.fabs(m.cos(m.pow(x,m.pow(3,n))))))
 	print (answer)
