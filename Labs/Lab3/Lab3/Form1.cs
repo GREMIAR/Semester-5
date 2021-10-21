b0c685d1e1f5362507d4662cefc69ce41d2badae
GREBIAR <nikitos.muzalevskiy@mail.ru>
2021-10-21 10:54:00 +0300

    add 3

---
+++ Labs/Lab3/Lab3/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.OleDb;
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1(string[] str)
+        {
+            DataTable dataTable;
+            InitializeComponent();
+            if (str.Length>0)
+            {
+                dataTable = ReadExcelFile.ReadExcel(str[0].Substring(0, str[0].LastIndexOf('.')), str[0].Substring(str[0].LastIndexOf('.')));
+            }
+            else
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "|*.xlsx;.xls";
+                if(openFileDialog.ShowDialog() == DialogResult.OK)
+                { 
+                    dataTable = ReadExcelFile.ReadExcel(openFileDialog.FileName.Substring(0, openFileDialog.FileName.LastIndexOf('.')), openFileDialog.FileName.Substring(openFileDialog.FileName.LastIndexOf('.')));
+                }
+                else
+                {
+                    Application.Exit();
+                }
+            }
+        }
+    }
+}
